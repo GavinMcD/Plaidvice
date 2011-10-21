5dadf688cabe88afccee81b1efb0f0eaac978b84
GavinMcD <gavin@brightmonk.com>
2011-10-20 22:06:36 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plaidvice::Application.config.secret_token = '4c1867070a794fa9e8a8e094f6da25357c32539f784b00faf4f186a2e15258ef1565ae6036b070143afac39b94aafc44b0eb43dd878cebf73dd51c7d1743afec'
